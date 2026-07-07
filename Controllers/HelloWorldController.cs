e25cc6eef3c6e29fe25003e5f88cb4ba7b21f603
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2026-07-07 12:41:30 +0100

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+	//Every public method in a controller is callable as an HTTP endpoint.
+	//Below, both methods return a string. 
+	//
+	// GET: /HelloWorld/
+	public IActionResult Index()
+	{
+		return View();
+	}
+	// 
+	// GET: /HelloWorld/Welcome/ 
+	// Requires using System.Text.Encodings.Web;
+	public string Welcome(string name, int ID = 1)
+	{
+		return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+	}
+}
